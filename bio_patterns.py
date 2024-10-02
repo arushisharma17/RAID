db06ae879748b7deb364b49321e045dd808432ee
zeyogh <zeynep.ogh@gmail.com>
2024-10-02 07:34:26 -0500

    refactor bio_patterns

--- bio_patterns.py
+++ bio_patterns.py
@@ -10,11 +10,9 @@ import graphviz
 def get_leaf_nodes(node):
     leaf_nodes = []
 
-    # Base case: If the node has no children, it is a leaf node
     if len(node.children) == 0:
         leaf_nodes.append(node)
 
-    # Recursively check the children for leaf nodes
     for child in node.children:
         leaf_nodes.extend(get_leaf_nodes(child))
 
@@ -27,13 +25,44 @@ def find_bio_label_type(l):
     return l.type
 
 
-def main():
+def bio_label_str(source_code, language):
     # Load Tree-sitter Java language
-    # JAVA_LANGUAGE = Language(tsjava.language())
-    # parser = Parser(JAVA_LANGUAGE)
-    PYTHON_LANGUAGE = Language(tspython.language())
-    parser = Parser(PYTHON_LANGUAGE)
+    if language == 'java':
+        JAVA_LANGUAGE = Language(tsjava.language())
+        parser = Parser(JAVA_LANGUAGE)
+    else:
+        PYTHON_LANGUAGE = Language(tspython.language())
+        parser = Parser(PYTHON_LANGUAGE)
 
+    # Parse the source code
+    tree = parser.parse(source_code)
+    root_node = tree.root_node
+    leaf_nodes = get_leaf_nodes(root_node)
+    bio = []
+    prev = None
+    o_type = None
+    for i, l in enumerate(leaf_nodes):
+        name = find_bio_label_type(l)
+        leaf_text = str(l.text)[2:-1]
+        # print("Leaf:", leaf_text, "\tName:", name, "\tType:", l.type, "\tParent Type:", l.parent.type, "Prev:", prev)
+        if l.type == leaf_text and i > 0 and (prev != name or o_type is None):
+            if ((language == "python" and l.parent.child_count == 2)
+                    or (language == "java" and l.parent.child_count == 3 and l.parent.child(2).type == ';')):
+                bio.append(leaf_text + ': B' + '-' + name)
+            else:
+                bio.append(leaf_text + ': O' + '-' + name)
+            prev = None
+            o_type = name
+        else:
+            bio_type = 'B' if prev != name else 'I'
+            bio.append(leaf_text + ': ' + bio_type + '-' + name)
+            prev = name
+            o_type = None
+
+    print(bio)
+
+
+def main():
     # source_code = b'''
     # public class HelloWorld {
     #     public static void main(String[] args) {
@@ -46,61 +75,24 @@ def main():
     # for (int i = 0; i < 10; i++) {
     #     System.out.println(i);
     # }
-    # '''
+    # # '''
 
-    source_code = b'''
-    def add_numbers (a, b):
-        return a + b
-    '''
+    # source_code = b'''
+    # def add_numbers (a, b):
+    #     return a + b
+    # '''
 
     # source_code = b'''
     # # Comment about function
     # '''
 
-    # source_code = b'''
-    # public addNumbers(a, b) {
-    #     return a + b;
-    # }
-    # '''
+    source_code = b'''
+    public addNumbers(a, b) {
+        return a + b;
+    }
+    '''
 
-    # Parse the source code
-    tree = parser.parse(source_code)
-    root_node = tree.root_node
-    leaf_nodes = get_leaf_nodes(root_node)
-    bio = []
-    prev = None
-    O_type = None
-    for i, l in enumerate(leaf_nodes):
-        name = find_bio_label_type(l)
-        leaf_text = str(l.text)[2:-1]
-        # print("Leaf:", leaf_text, "\tName:", name, "\tType:", l.type, "\tParent Type:", l.parent.type, "Prev:", prev)
-        # _, b, _ = extract_tokens_with_bio(l)
-        if l.type == leaf_text and i > 0 and (prev != name or O_type is None):
-            if l.parent.child_count == 2:
-                bio.append(leaf_text + ': B' + '-' + name)
-            else:
-                bio.append(leaf_text + ': O' + '-' + name)
-            prev = None
-            O_type = name
-        else:
-            bio_type = 'B' if prev != name else 'I'
-            bio.append(leaf_text + ': ' + bio_type + '-' + name)
-            prev = name
-            O_type = None
-
-
-    print(bio)
-
-    # tokens, bio_labels, leaf_labels = extract_tokens_with_bio(root_node)
-
-    # print(tokens[:-(len(leaf_nodes))], "\n", bio_labels[:-(len(leaf_nodes))])
-
-    # Print tokens and corresponding BIO labels
-    # print("\nExtracted Tokens and BIO Labels:")
-    # for token, bio in zip(tokens, bio_labels):
-    #     # print(f"Token: {token}, BIO Label: {bio}")
-    #     print(f"{token}, {bio}")
-    # print(leaf_labels)
+    bio_label_str(source_code, "java")
 
 
 if __name__ == "__main__":
