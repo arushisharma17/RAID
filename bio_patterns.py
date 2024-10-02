9def0f6f09cac54702df36244c2e08cd4c5f6c31
zeyogh <zeynep.ogh@gmail.com>
2024-10-02 09:12:30 -0500

    refactor bio_patterns, clean output

--- bio_patterns.py
+++ bio_patterns.py
@@ -7,7 +7,7 @@ import pandas as pd
 import graphviz
 
 
-def get_nodes_at_level(node, target_level, current_level=0):
+def get_nodes_at_level(node, target_level=-1, current_level=0):
     nodes_at_level = []
 
     # if level is beyond tree, appends the leaf
@@ -21,7 +21,7 @@ def get_nodes_at_level(node, target_level, current_level=0):
 
 
 def find_bio_label_type(l):
-    if (l.type == str(l.text)[2:-1] or l.type == "identifier") and l.parent is not None:
+    if (l.type == str(l.text)[2:-1] or l.type == 'identifier') and l.parent is not None:
         return l.parent.type
     return l.type
 
@@ -37,7 +37,7 @@ def bio_label_str(source_code, language):
     # Parse the source code
     tree = parser.parse(source_code)
     root_node = tree.root_node
-    leaf_nodes = get_nodes_at_level(root_node, 5)
+    leaf_nodes = get_nodes_at_level(root_node)
     bio = []
     prev = None
     o_type = None
@@ -46,20 +46,23 @@ def bio_label_str(source_code, language):
         leaf_text = str(l.text)[2:-1]
         # print("Leaf:", leaf_text, "\tName:", name, "\tType:", l.type, "\tParent Type:", l.parent.type, "Prev:", prev)
         if l.type == leaf_text and i > 0 and (prev != name or o_type is None):
-            if ((language == "python" and l.parent.child_count == 2)
-                    or (language == "java" and l.parent.child_count == 3 and l.parent.child(2).type == ';')):
-                bio.append(leaf_text + ': B' + '-' + name)
+            if ((language == 'python' and l.parent.child_count == 2)
+                    or (language == 'java' and l.parent.child_count == 3 and l.parent.child(2).type == ';')):
+                bio.append(leaf_text + ": B" + '-' + name)
             else:
-                bio.append(leaf_text + ': O' + '-' + name)
+                bio.append(leaf_text + ": O" + '-' + name)
             prev = None
             o_type = name
         else:
             bio_type = 'B' if prev != name else 'I'
-            bio.append(leaf_text + ': ' + bio_type + '-' + name)
+            bio.append(leaf_text + ": " + bio_type + '-' + name)
             prev = name
             o_type = None
 
-    print(bio)
+    print('{0:11}  {1}'.format('TOKEN', 'LABEL'))
+    for element in bio:
+        split_element = element.split(':')
+        print('{0:10}  {1}'.format(split_element[0], split_element[1]))
 
 
 def main():
@@ -87,12 +90,12 @@ def main():
     # '''
 
     source_code = b'''
-    public addNumbers(a, b) {
+    public int addNumbers(a, b) {
         return a + b;
     }
     '''
 
-    bio_label_str(source_code, "java")
+    bio_label_str(source_code, 'java')
 
 
 if __name__ == "__main__":
