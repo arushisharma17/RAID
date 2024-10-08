30f5814a74951c05659fbb118fcfcef40175c20c
zeyogh <zeynep.ogh@gmail.com>
2024-10-07 20:03:35 -0500

    read_data from text file instead, generate .in and .label

--- read_data.py
+++ read_data.py
@@ -16,8 +16,9 @@ def read_file(file_name):
 
 
 def write_file(file_name, strings, tokens, labels):
+    file = file_name[:-4]
     prev = 0
-    with open(file_name, 'a') as file:
+    with open(file + '.in', 'a') as file_in, open(file + '.label', 'a') as file_labels:
         lines = strings.split('\n')
         token_index = 0
         for l in lines:
@@ -30,22 +31,29 @@ def write_file(file_name, strings, tokens, labels):
             if count == 0:
                 count = count + 1
             for i in range(prev, count + prev):
-                file.write(labels[i] + ' ')
-            file.write('\n')
+                file_in.write(tokens[i] + ' ')
+                file_labels.write(labels[i] + ' ')
+            file_in.write('\n')
+            file_labels.write('\n')
             prev = prev + count
             token_index = 0
-        file.write('\n')
+        file_in.write('\n')
+        file_labels.write('\n')
 
+
+def generate_in_and_label_files(file_name, language):
+    strings = read_file(file_name)
+    file = file_name[:-4]
+    with open(file + '.in', 'w') as file_in, open(file + '.label', 'w') as file_label:
+        file_in.write('')
+        file_label.write('')
+    for st in strings:
+        tokens, labels, _ = extract_bio_labels_from_source_code(bytes(st, encoding='utf8'), language)
+        write_file(file_name, st, tokens, labels)
 
 
 def main():
-    strings = read_file('java.in')
-    print(strings)
-    with open('java.label', 'w') as file:
-        file.write('')
-    for st in strings:
-        tokens, labels, _ = extract_bio_labels_from_source_code(bytes(st, encoding='utf8'), 'java')
-        write_file('java.label', st, tokens, labels)
+    generate_in_and_label_files('java.txt', 'java')
 
 
 if __name__ == "__main__":
