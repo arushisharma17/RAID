4b0614c37ba34fecf230b4e8f3c9138620b05d70
rishihk <hk.hrishi30@gmail.com>
2024-12-09 20:27:25 -0600

    running some tests

--- input/sample_input.java
+++ input/sample_input.java
@@ -1,7 +1,6 @@
 
-public class HelloWorld {
+public class HelloWorlds {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
         System.out.println("Hello, World!");
     }
 }
